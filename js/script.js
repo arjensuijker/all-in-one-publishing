02567494ab93b5189038d84ab97786d00d90d5a8
arjensuijker <arjensuijker@gmail.com>
2015-05-20 16:20:19 +0200

    Used javascript to move all footnotes to into their respective links, placing them in small pop-up windows.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ $(function() {
     $('.footnoteRef').each(function(){
 		var number = $(this).find('sup').html();
 		var footnoteContent=$("#fn"+ number + " p").html();
+		console.log(footnoteContent);
 		$("#fn"+ number).remove();
-		$(this).find(".footnote-tooltip").html(footnoteContent);
+		$(this).append("<span class='footnote-tooltip'>"+footnoteContent+"</span>");
 	})	
 });
