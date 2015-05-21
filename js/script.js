953979a951f87de2bad9a0235507396067c4694c
arjensuijker <arjensuijker@gmail.com>
2015-05-21 15:54:57 +0200

    Added mobile styling with media queries. It somewhat imitates the behaviour of native apps, but I tried to keep it platform indipendent.

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@ $(function() {
 		var number = $(this).find('sup').html();
 		var footnote = $(this).closest(".essay").find("#fn"+ number);
 		var footnoteContent=footnote.html();
-		console.log(footnoteContent);
 		footnote.remove();
 		$(this).append("<span class='footnote-tooltip'>"+footnoteContent+"</span>");
 	})	
+	$('.toggle-menu').click(function(){
+		$('nav.index').toggleClass('open');
+	})
 });
