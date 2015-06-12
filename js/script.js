fde21d48dad5321268c7c64407538d1519d4aaeb
arjensuijker <arjensuijker@gmail.com>
2015-06-12 16:42:09 +0200

    print tweaks after checking printed version

--- js/script.js
+++ js/script.js
@@ -4,18 +4,5 @@ $(function() {
 		var footnote = $(this).closest(".essay").find("#fn"+ number);
 		var footnoteContent=footnote.html();
 		$(this).append("<span class='footnote-tooltip'>"+footnoteContent+"</span>");
-	})	
-	$('.footnotes').hide();    
-});
-
-var mediaQueryList = window.matchMedia(' print ');
-mediaQueryList.addListener(function(mql) {
-	console.log('print');
-    if (mql.matches) {
-		$('.footnotes').show();
-		$('.footnote-tooltip').hide();
-    }else{
-		$('.footnotes').hide(); 
-		$('.footnote-tooltip').show();		
-	}
+	})	  
 });
