30d04a48a085b845fc5e552a9ea8802a246454ae
arjensuijker <arjensuijker@gmail.com>
2015-07-08 09:58:35 +0200

    i forgot

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(function() {
-	$('.footnoteRef').each(function(){
-		var number = $(this).find('sup').html();
-		var footnote = $(this).closest(".essay").find("#fn"+ number);
-		var footnoteContent=footnote.html();
-		$(this).append("<span class='footnote-tooltip'>"+footnoteContent+"</span>");
-	})	  
+	//$('.footnoteRef').each(function(){
+		//var number = $(this).find('sup').html();
+		//var footnote = $(this).closest(".essay").find("#fn"+ number);
+		//var footnoteContent=footnote.html();
+		//$(this).append("<span class='footnote-tooltip'>"+footnoteContent+"</span>");
+	//})	  
 });
